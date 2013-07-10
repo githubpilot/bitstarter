64ed78277e6cb5f4fe5cd8f08e1c90763ae83d8a
Liam Toolan <liamtoolan@gmail.com>
2013-07-10 22:06:02 +0000

    HW3 1-7 Added index.html readFileSync and buffer.toString

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var file_string = fs.readFileSync('index.html');
+  response.send(file_string.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
